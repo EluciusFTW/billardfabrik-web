8590a4ffc4772ffe6b9feae4439e22097904fcbc
EluciusFTW <guysbuss@gmail.com>
2022-01-30 14:09:57 +0100

    Missing renaming

--- src/app/tourneys/tourneys.component.ts
+++ src/app/tourneys/tourneys.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 
 @Component({
-  templateUrl: './billard-fabrik-tourneys.component.html'
+  templateUrl: 'tourneys.component.html'
 })
 export class BillardFabrikTourneysComponent { }
