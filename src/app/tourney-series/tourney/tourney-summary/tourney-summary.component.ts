0d91d7c9513a2bcc0a15b851c47898af4261ef42
EluciusFTW <guysbuss@gmail.com>
2022-04-05 21:09:45 +0200

    Moved adding a player to the suummary page

--- src/app/tourney-series/tourney/tourney-summary/tourney-summary.component.ts
+++ src/app/tourney-series/tourney/tourney-summary/tourney-summary.component.ts
@@ -6,6 +6,12 @@ import { PoolDisciplineMapper } from '../../models/pool-discipline';
 import { TourneyEvaluationService } from '../../services/tourney-evaluation-service';
 import { TourneyStatisticsService } from '../../services/tourney-statistics.service';
 import { PlayersService } from '../../services/players.service';
+import { TourneyPhaseStatus } from '../../models/tourney-phase-status';
+import { MatDialog } from '@angular/material/dialog';
+import { TourneyGroupStageAddPlayerDialogComponent } from '../tourney-group-stage/tourney-group-stage-add-player-dialog.component';
+import { TourneyGroup } from '../../models/tourney-group';
+import { Match } from '../../models/match';
+import { MatchStatus } from '../../models/match-status';
 // import { UserService } from 'src/app/authenticated-area/user.service';
 
 @Component({
@@ -27,13 +33,64 @@ export class TourneySummaryComponent {
     private evaluationService: TourneyEvaluationService,
     private statisticsService: TourneyStatisticsService,
     private playersService: PlayersService,
+    public dialog: MatDialog,
     // private userService: UserService
   ) { }
+
+  addPlayer(): void {
+    const dialogRef = this.dialog.open(TourneyGroupStageAddPlayerDialogComponent, {
+      data: {},
+      width: '300px',
+      hasBackdrop: true
+    });
+
+    dialogRef.afterClosed().subscribe(
+      name => {
+        if (name) {
+          this.inject(name)
+        }
+      }
+    )
+  }
+
+  inject(name: any) {
+    let group = this.chooseRandomGroup()
+    this.addMatches(name, group);
+    group.players.push(name);
+    this.change.emit(TourneyPhaseEvent.scoreChanged);
+  }
+
+  chooseRandomGroup(): TourneyGroup {
+    var smallestGroupsize = this.tourney.groups.map(group => group.players.length).sort()[0];
+    const viableGroups = this.tourney.groups.filter(group => group.players.length === smallestGroupsize);
+    let chosenGroup = viableGroups[Math.floor(Math.random() * viableGroups.length)];
+
+    return chosenGroup;
+  }
+
+  addMatches(newPlayerName: any, group: TourneyGroup) {
+    var referenceMatch = group.matches[0];
+    const matches = group.players
+      .map(player => <Match>
+      {
+        playerOne: { name: newPlayerName, points: 0 },
+        playerTwo: { name: player, points: 0 },
+        discipline: referenceMatch.discipline,
+        length: referenceMatch.length,
+        status: MatchStatus.notStarted
+      });
+    group.matches.push(...matches);
+  }
 
   displayStatus(): string {
     return this.tourney
       ? this._mapper.map(this.tourney.meta.status)
       : 'Loading ...';
+  }
+
+  canAddPlayers(): boolean {
+    return true;
+    // return this.userService.canHandleTourneys();
   }
 
   getDiscipline() {
@@ -64,6 +121,10 @@ export class TourneySummaryComponent {
       }
       this.change.emit(TourneyPhaseEvent.resultsPostProcessed);
     }
+  }
+
+  someGroupFinished(): boolean {
+    return this.tourney.groups.some(group => group.status === TourneyPhaseStatus.finalized)
   }
 
   canStart(): boolean {
