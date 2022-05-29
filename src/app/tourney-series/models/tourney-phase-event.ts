9a7ba3699800ce91449c4ac9a9094e94a702afac
EluciusFTW <guysbuss@gmail.com>
2022-05-29 10:25:12 +0200

    Remodelled the phase events

--- src/app/tourney-series/models/tourney-phase-event.ts
+++ src/app/tourney-series/models/tourney-phase-event.ts
@@ -1,3 +1,58 @@
+import { TourneyDoubleEliminationStageType } from "./tourney-double-elimination-stage";
+import { TourneyEliminationStageType } from "./tourney-elimination-stage"
+
+export type EventType =
+  | 'Created'
+  | 'Started'
+  | 'ScoreChanged'
+  | 'GroupStageFinalized'
+  | 'SingleEliminationStageFinalized'
+  | 'DoubleEliminationStageFinalized'
+  | 'ResultsPostProcessed'
+
+export abstract class TourneyEventBase {
+  abstract type: EventType
+}
+
+export class CreationEvent extends TourneyEventBase {
+  type: 'Created';
+}
+
+export class StartEvent extends TourneyEventBase {
+  type: 'Started';
+}
+
+export class ScoreChangedEvent extends TourneyEventBase {
+  type: 'ScoreChanged';
+}
+
+export class GroupStageFinalizedEvent extends TourneyEventBase {
+  type: 'GroupStageFinalized';
+}
+
+export class SingleEliminationStageFinalizedEvent extends TourneyEventBase {
+  type: 'SingleEliminationStageFinalized';
+  stage: TourneyEliminationStageType;
+}
+
+export class DoubleEliminationStageFinalizedEvent extends TourneyEventBase {
+  type: 'DoubleEliminationStageFinalized';
+  stage: TourneyDoubleEliminationStageType;
+}
+
+export class ResultsPostProcessedEvent extends TourneyEventBase {
+  type: 'ResultsPostProcessed';
+}
+
+export type TPE =
+  | CreationEvent
+  | StartEvent
+  | ScoreChangedEvent
+  | GroupStageFinalizedEvent
+  | DoubleEliminationStageFinalizedEvent
+  | SingleEliminationStageFinalizedEvent
+  | ResultsPostProcessedEvent
+
 export enum TourneyPhaseEvent{
     created = 0,
     started = 1,
--- src/app/tourney-series/services/tourney-event.service.ts
+++ src/app/tourney-series/services/tourney-event.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Tourney } from '../models/tourney';
-import { TourneyPhaseEvent } from '../models/tourney-phase-event';
+import { TourneyPhaseEvent, TPE } from '../models/tourney-phase-event';
 import { TourneyPhaseStatus } from '../models/tourney-phase-status';
 import { TourneyStatus } from '../models/tourney-status';
 import { TourneyEliminationStageFinalizedService } from './tourney-elimination-stage-finalized.service';
@@ -39,7 +39,33 @@ export class TourneyEventService {
       }
     }
   }
-  
+
+  // apply(tourney: Tourney, event: TPE): void {
+  //   switch (event.type) {
+  //     case 'Started': {
+  //       this.startTourney(tourney);
+  //       return;
+  //     }
+  //     case 'GroupStageFinalized' : {
+  //       this.groupStageFinalizedService.handle(tourney);
+  //       return;
+  //     }
+  //     case 'SingleEliminationStageFinalized' : {
+  //       this.eliminationStageFinalizedService.handle(tourney);
+  //       return;
+  //     }
+  //     case 'DoubleEliminationStageFinalized' : {
+  //       this.eliminationStageFinalizedService.handle(tourney);
+  //       return;
+  //     }
+
+  //     case 'ResultsPostProcessed' : {
+  //       tourney.meta.status = TourneyStatus.postProcessed;
+  //       return;
+  //     }
+  //   }
+  // }
+
   startTourney(tourney: Tourney) {
     tourney.meta.status = TourneyStatus.ongoing;
     tourney.groups.forEach(group => group.status = TourneyPhaseStatus.readyOrOngoing);
