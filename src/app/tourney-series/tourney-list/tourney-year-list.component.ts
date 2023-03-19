24ed4ef869ed9e670c65d0c45d6e599034d13ae5
EluciusFTW <guysbuss@gmail.com>
2023-03-19 11:43:46 +0100

    Added show and comupute fucntionality to tourney list

--- src/app/tourney-series/tourney-list/tourney-year-list.component.ts
+++ src/app/tourney-series/tourney-list/tourney-year-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from "@angular/core";
+import { MatDialog } from "@angular/material/dialog";
 import { MatTableDataSource } from "@angular/material/table";
 import { Subscription } from "rxjs";
 import { UserService } from "src/app/authentication/user.service";
@@ -7,6 +8,7 @@ import { TourneyStatus, TourneyStatusMapper } from "../models/tourney-status";
 import { TourneyStatisticsService } from "../services/evaluation/tourney-statistics.service";
 import { PlayersService } from "../services/players.service";
 import { TourneysService } from "../services/tourneys.service";
+import { ShowResultsDialogComponent } from "./show-results.dialog.component";
 
 @Component({
   selector: 'app-tourney-year-list',
@@ -20,13 +22,14 @@ export class TourneyYearListComponent implements OnInit {
   private tourneysSub: Subscription;
 
   tourneyDataSource: MatTableDataSource<Tourney>;
-  displayedColumns = ['name', 'date', 'status']; // , 'actions' for evaluating tourney
+  displayedColumns = ['name', 'date', 'status', 'actions'];
 
   constructor(
     private tourneysService: TourneysService,
     private statisticsService: TourneyStatisticsService,
     private playersService: PlayersService,
-    private userService: UserService) { }
+    private userService: UserService,
+    private dialog: MatDialog) { }
 
   ngOnInit(): void {
     this.tourneysSub = this.tourneysService
@@ -41,15 +44,20 @@ export class TourneyYearListComponent implements OnInit {
       )
   }
 
+  show(tourney: Tourney) {
+    var result = this.statisticsService.Evaluate(tourney);
+    this.dialog.open(ShowResultsDialogComponent, { data: result })
+  }
+
   calculate(tourney: Tourney) {
-    if (tourney.meta.status === TourneyStatus.completed) {
-      var result = this.statisticsService.Evaluate(tourney);
-      if (result) {
-        result.players.forEach(evaluation => this.playersService.AddPlayerRecord(evaluation));
-      }
-      // ToDo: Save tourney changed state
-      // tourney.meta.status = TourneyStatus.postProcessed;
-      //this.change.emit({ type: 'ResultsPostProcessed' });
+    if (tourney.meta.status !== TourneyStatus.completed) {
+      return;
+    }
+
+    var result = this.statisticsService.Evaluate(tourney);
+    if (result) {
+      result.players.forEach(evaluation => this.playersService.AddPlayerRecord(evaluation));
+      this.tourneysService.update(tourney, { type: 'ResultsPostProcessed' });
     }
   }
 
@@ -62,6 +70,11 @@ export class TourneyYearListComponent implements OnInit {
 
   isTourneyAuthenticated(): boolean {
     return this.userService.canHandleTourneys();
+  }
+
+  canComplete(status: TourneyStatus): boolean {
+    return this.userService.canHandleTourneys()
+      && status === TourneyStatus.completed;
   }
 
   mapTourneyState(status: TourneyStatus) {
