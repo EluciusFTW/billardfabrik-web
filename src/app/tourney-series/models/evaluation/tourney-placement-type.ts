5389eaf9181dbf756f5cc0ce687c2e79b741aae1
EluciusFTW <guysbuss@gmail.com>
2023-03-19 10:11:43 +0100

    Added tourney placement type mapping

--- src/app/tourney-series/models/evaluation/tourney-placement-type.ts
+++ src/app/tourney-series/models/evaluation/tourney-placement-type.ts
@@ -1,12 +1,4 @@
-export enum TourneyPlacementTypeOld {
-  GroupStage = 0,
-  EigthFinal = 1,
-  QuarterFinal = 2,
-  FourthPlace = 3,
-  ThirdPlace = 4,
-  RunnerUp = 5,
-  Winner = 6
-}
+import { TourneyMode } from "../tourney-mode";
 
 export enum TourneyPlacementType {
   First = 10,
@@ -25,4 +17,53 @@ export enum TourneyPlacementType {
   Last128 = 23,
   Last256 = 24,
   Group = 50,
+}
+
+export function map(placement: TourneyPlacementType, mode: TourneyMode): string {
+  switch (placement) {
+    case TourneyPlacementType.First:
+      return 'Gewinner';
+    case TourneyPlacementType.Second:
+      return 'Zweiter';
+    case TourneyPlacementType.Third:
+      return 'Dritter';
+    case TourneyPlacementType.Fourth:
+      return 'Vierter';
+    case TourneyPlacementType.Last6:
+      return '5.-6. Platz';
+    case TourneyPlacementType.Last8:
+      return mode === TourneyMode.DoubleElimination
+        ? '7.-8. Platz'
+        : '5.-8.';
+    case TourneyPlacementType.Last12:
+      return '9.-12. Platz';
+    case TourneyPlacementType.Last16:
+      return mode === TourneyMode.DoubleElimination
+        ? '13.-16. Platz'
+        : '9.-16. Platz';
+    case TourneyPlacementType.Last24:
+      return '17.-24. Platz';
+    case TourneyPlacementType.Last32:
+      return mode === TourneyMode.DoubleElimination
+        ? '25.-32. Platz'
+        : '17.-32. Platz';
+    case TourneyPlacementType.Last48:
+      return '33.-48. Platz';
+    case TourneyPlacementType.Last64:
+      return mode === TourneyMode.DoubleElimination
+        ? '49.-64. Platz'
+        : '33.-64. Platz';
+    case TourneyPlacementType.Last96:
+      return '65.-96. Platz';
+    case TourneyPlacementType.Last128:
+      return mode === TourneyMode.DoubleElimination
+        ? '65.-128. Platz'
+        : '97.-128. Platz';
+    case TourneyPlacementType.Last256:
+      return '129.-256. Platz';
+    case TourneyPlacementType.Group:
+      return 'Gruppenphase';
+    default:
+      return 'Unbekannte Plazierung';
+  }
 }
