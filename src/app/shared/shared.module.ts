196d17d6597827037c5fa4ab46d8ec085c712f44
EluciusFTW <guysbuss@gmail.com>
2023-09-02 13:20:41 +0200

    Removed obsolete components

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,9 +2,7 @@ import { RouterModule } from '@angular/router';
 import { OwnMessageService } from './services/own-message.service';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ContentHeaderComponent } from './content-header/content-header.component';
 import { ContentTileComponent } from './content-tile/content-tile.component';
-import { TimeTickPipe } from './pipes/time-tick.pipe';
 import { ConfirmDialogComponent } from './confirm-dialog/confirm-dialog.component';
 import { OwnConfirmService } from './services/own-confirm.service';
 import { MaterialModule } from '../material/material.module';
@@ -16,15 +14,11 @@ import { MaterialModule } from '../material/material.module';
         MaterialModule
     ],
     declarations: [
-        ContentHeaderComponent,
         ContentTileComponent,
-        TimeTickPipe,
         ConfirmDialogComponent
     ],
     exports: [
-        ContentHeaderComponent,
         ContentTileComponent,
-        TimeTickPipe
     ],
     providers: [
         OwnMessageService,
