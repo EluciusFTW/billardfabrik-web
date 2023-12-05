a14b9e27248530523fced28096a83e094aa8dea0
EluciusFTW <guysbuss@gmail.com>
2023-12-05 15:41:11 +0100

    Beautified the matches page

--- src/app/ranking/ranking-matches/ranking-matches.component.ts
+++ src/app/ranking/ranking-matches/ranking-matches.component.ts
@@ -1,12 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
-import { EloService } from '../elo.service';
+import { EloService, RankingMatch } from '../elo.service';
 import { firstValueFrom, take } from 'rxjs';
 import { TourneyFunctions } from 'src/app/tourney-series/tourney/tourney-functions';
 import { Match } from 'src/app/tourney-series/models/match';
 import { CompletedMatch, EloFunctions } from 'src/app/tourney-series/services/evaluation/elo-functions';
-
-type RankingMatch = Match & {date: string, diff?: number }
 
 @Component({
   selector: 'app-ranking-matches',
@@ -15,7 +13,7 @@ type RankingMatch = Match & {date: string, diff?: number }
 })
 export class RankingMatchesComponent implements OnInit {
   rankingMatches: RankingMatch[];
-  dataSource = new MatTableDataSource<Match>();
+  dataSource = new MatTableDataSource<RankingMatch>();
   displayedColumns = ['date', 'p1', 'p2', 'score', 'diff'];
 
   constructor(private eloService: EloService) { }
@@ -28,10 +26,10 @@ export class RankingMatchesComponent implements OnInit {
         matches => {
           this.rankingMatches = matches
             .map(m => ({
-                date: TourneyFunctions
-                  .NameFragmentToDate(m.key.substring(0,8))
-                  .toLocaleDateString(),
-                ... m}))
+                ... m,
+                p1: m.playerOne.name,
+                p2: m.playerTwo.name
+            }))
             .reverse();
           this.dataSource = new MatTableDataSource(this.rankingMatches);
         }
