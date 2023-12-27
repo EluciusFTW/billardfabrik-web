0a19a30d0879e4bf43e8625618bdeda2563ac3ea
EluciusFTW <guysbuss@gmail.com>
2023-12-27 16:28:53 +0100

    Moved main import functionality to tourney impurt component

--- src/app/ranking/import-matches/import-tourney/import-tourney.component.ts
+++ src/app/ranking/import-matches/import-tourney/import-tourney.component.ts
@@ -3,27 +3,68 @@ import { EloService } from '../../elo.service';
 import { TourneysService } from 'src/app/tourney-series/services/tourneys.service';
 import { Tourney } from 'src/app/tourney-series/models/tourney';
 import { take } from 'rxjs';
+import { MatTableDataSource } from '@angular/material/table';
+import { TourneyStatus, TourneyStatusMapper } from 'src/app/tourney-series/models/tourney-status';
+import { EloImportService } from '../../elo-import.service';
 
 @Component({
   selector: 'app-import-tourney',
-  templateUrl: './import-tourney.component.html',
-  styleUrls: ['./import-tourney.component.scss']
+  templateUrl: './import-tourney.component.html'
 })
 export class ImportTourneyComponent implements OnInit {
 
   lastImported = ''
-  unimportedTourneys: Tourney[] = [];
-  
+  noTourneys = false;
+  unimportedTourneys: MatTableDataSource<Tourney>;
+  displayedColumns = ['name', 'date', 'status'];
+
   constructor(
     private readonly eloService: EloService,
+    private readonly eloImportService: EloImportService,
     private readonly tourneysService: TourneysService) {
   }
 
   async ngOnInit() {
+    await this.loadData();
+  }
+
+  private async loadData(): Promise<void> {
     this.lastImported = await this.eloService.GetLastTourneyDate();
     this.tourneysService
-      .getBetween(this.lastImported, '20990101')
+      .getAfter(this.lastImported)
       .pipe(take(1))
-      .subscribe(tourneys => this.unimportedTourneys = tourneys);
+      .subscribe(tourneys => {
+        if(tourneys.length > 0){
+          this.noTourneys = false;
+          this.unimportedTourneys = new MatTableDataSource<Tourney>(tourneys);
+        }
+      });
+  }
+
+  mapTourneyState(status: TourneyStatus) {
+    return TourneyStatusMapper.map(status);
+  }
+
+  importable(tourney: Tourney): boolean {
+    return tourney.meta.status === TourneyStatus.postProcessed;
+  }
+
+  async importBatch() {
+    const imports = this.unimportedTourneys.data.map(tourney => this.importTourney(tourney));
+
+    await Promise.all(imports);
+    await this.loadData();
+  }
+
+  async reset() {
+    await this.eloService.reset();
+  }
+
+  private async importTourney(tourney: Tourney): Promise<void> {
+    let importedPlayers = await this.eloImportService.ImportPlayers(tourney);
+    console.log('Imported Players: ', importedPlayers);
+
+    let importedMatches = await this.eloImportService.ImportMatches(tourney);
+    console.log('Imported Matches: ', importedMatches);
   }
 }
