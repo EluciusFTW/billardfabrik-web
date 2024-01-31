ff82528e9fe5c9ab5f57b250774ee66a69604a60
EluciusFTW <guysbuss@gmail.com>
2024-01-31 15:58:11 +0100

    Added player management to menu

--- src/app/ranking/ranking-routing.module.ts
+++ src/app/ranking/ranking-routing.module.ts
@@ -6,6 +6,7 @@ import { RankingMatchesComponent } from './ranking-matches/ranking-matches.compo
 import { ScoringDetailsComponent } from './scoring-details/scoring-details.component';
 import { ImportMatchesComponent } from './import-matches/import-matches.component';
 import { UserGuard } from '../authentication/guards/user.guard';
+import { PlayerListingComponent } from '../players/player-listing/player-listing.component';
 
 const routes: Routes = [{
   path: 'ranking',
@@ -16,6 +17,7 @@ const routes: Routes = [{
     { path: 'matches', component: RankingMatchesComponent },
     { path: 'details', component: ScoringDetailsComponent },
     { path: 'import', component: ImportMatchesComponent, canActivate: [UserGuard] },
+    { path: 'player-management', component: PlayerListingComponent, canActivate: [UserGuard] }
   ]
 }];
 
