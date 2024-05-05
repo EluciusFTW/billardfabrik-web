e1bfb91437ad14d6ea5fa2517fa38b8871fb9516
EluciusFTW <guysbuss@gmail.com>
2024-05-05 20:55:55 +0200

    tourney group component - migrated to signals

--- src/app/tourney-series/tourney-group/tourney-group.component.ts
+++ src/app/tourney-series/tourney-group/tourney-group.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Output, computed, effect, inject, input } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { GroupStanding } from '../models/group-standing';
 import { Match } from '../models/match';
@@ -14,27 +14,29 @@ import { GroupFunctions } from './group-functions';
   templateUrl: './tourney-group.component.html',
   styleUrls: ['../tourneys.scss', './tourney-group.component.scss']
 })
-export class TourneyGroupComponent implements OnInit {
+export class TourneyGroupComponent {
+  private userService = inject(UserService);
 
-  @Input({ required: true })
-  group: TourneyGroup
+  group = input.required<TourneyGroup>();
 
   @Output()
   change: EventEmitter<TourneyPhaseEvent> = new EventEmitter();
   displayedColumnsMatches = ['p1', 'p2', 'score', 'cancel'];
 
-  standing: GroupStanding[] = [];
-  totals = new MatTableDataSource<GroupStanding>(this.standing);
+  standing = computed(() => GroupFunctions.calculcateStanding(this.group()));
+  groupActive = computed(() => this.group().status === TourneyPhaseStatus.readyOrOngoing);
+  groupCompleted = computed(() => this.group().status == TourneyPhaseStatus.finalized);
+  allGamesOver = computed(() =>
+    this.group().matches
+      .filter(match => match.status !== MatchStatus.cancelled)
+      .findIndex(match => !Match.isOver(match)) === -1);
+
+  totals = new MatTableDataSource<GroupStanding>([]);
   displayedColumnsTotals = ['name', 'games', 'won', 'goals'];
 
-  constructor(private userService: UserService) { }
-
-  ngOnInit(): void {
-    this.calculateTotals();
-  }
+  calcTotals = effect(() => this.totals = new MatTableDataSource<GroupStanding>(this.standing()));
 
   emitChange($event: TourneyPhaseEvent): void {
-    this.calculateTotals();
     this.change.emit($event);
   }
 
@@ -42,49 +44,21 @@ export class TourneyGroupComponent implements OnInit {
     return this.userService.canHandleTourneys();
   }
 
-  getStandingClass(standing: GroupStanding): string {
-    return this.standing
-      .map(s => s.name)
-      .slice(0, 2)
-      .includes(standing.name)
-        ? 'running'
-        : '';
-  }
-
-  get groupActive() {
-    return this.group.status === TourneyPhaseStatus.readyOrOngoing;
-  }
-
-  get groupCompleted() {
-    return this.group.status == TourneyPhaseStatus.finalized;
-  }
-
-  allGamesOver(): boolean {
-    return this.group.matches
-      .filter(match => match.status !== MatchStatus.cancelled)
-      .findIndex(match => !Match.isOver(match)) === -1;
-  }
-
   finalize(): void {
     if (!this.allGamesOver()) {
       return;
     }
 
-    this.group.status = TourneyPhaseStatus.finalized;
-    this.group.matches
+    this.group().status = TourneyPhaseStatus.finalized;
+    this.group().matches
       .filter(match => match.status !== MatchStatus.cancelled)
       .forEach(match => match.status = MatchStatus.done);
 
-    this.group.qualified = GroupFunctions
-      .calculcateStanding(this.group)
+    this.group().qualified = GroupFunctions
+      .calculcateStanding(this.group())
       .slice(0, 2)
       .map(row => row.name);
 
     this.change.emit({ type: 'GroupFinalized' });
-  }
-
-  private calculateTotals(): void {
-    this.standing = GroupFunctions.calculcateStanding(this.group);
-    this.totals = new MatTableDataSource<GroupStanding>(GroupFunctions.calculcateStanding(this.group));
   }
 }
