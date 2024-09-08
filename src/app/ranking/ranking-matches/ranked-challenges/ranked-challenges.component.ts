13f995faf70c8d5a747f07b822c0ea7e029e72fa
EluciusFTW <guysbuss@gmail.com>
2024-09-08 17:39:51 +0200

    Added discipline display in ranked challenges

--- src/app/ranking/ranking-matches/ranked-challenges/ranked-challenges.component.ts
+++ src/app/ranking/ranking-matches/ranked-challenges/ranked-challenges.component.ts
@@ -13,10 +13,11 @@ export class RankedChallengesComponent implements OnInit {
   private readonly eloRankingService = inject(EloRankingService);
 
   dataSource = new MatTableDataSource<IncomingMatch>([]);
-  displayedColumns = ['date', 'p1', 'p2', 'score', 'diff'];
+  displayedColumns = ['date', 'discipline', 'p1', 'p2', 'score', 'diff'];
 
   async ngOnInit() {
     const matches = await firstValueFrom(this.eloRankingService.GetRankedChallenges(50));
+    console.log(matches[0]);
     this.dataSource = new MatTableDataSource(matches.reverse());
   }
 }
