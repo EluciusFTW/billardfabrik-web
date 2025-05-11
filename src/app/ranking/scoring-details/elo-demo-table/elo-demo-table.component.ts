aedd30a73b246c50d22d60dee98dff391717fe47
EluciusFTW <guysbuss@gmail.com>
2025-05-11 14:07:44 +0200

    Reworked elo demo table

--- src/app/ranking/scoring-details/elo-demo-table/elo-demo-table.component.ts
+++ src/app/ranking/scoring-details/elo-demo-table/elo-demo-table.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, computed, effect, input } from '@angular/core';
 import { MatTableDataSource } from '@angular/material/table';
 import { EloFunctions } from 'src/app/ranking/elo-functions';
 import { EloMode } from '../../models/elo-models';
@@ -8,41 +8,36 @@ import { EloMode } from '../../models/elo-models';
   templateUrl: './elo-demo-table.component.html',
   styleUrls: ['./elo-demo-table.component.scss']
 })
-export class EloDemoTableComponent implements OnInit {
+export class EloDemoTableComponent {
   dataSource = new MatTableDataSource<EloScoreDemo>();
-  displayedColumns = ['p1', 'p2', 's0', 's1','s2','s3','s4','s5','s6', 's7', 's8'];
+  displayedColumns = ['p1', 'p2', 's0', 's1', 's2', 's3', 's4', 's5', 's6', 's7', 's8'];
 
-  private readonly possibleOpponentScores = [0,1,2,3,4,5,6,7,8];
+  private readonly possibleOpponentScores = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
-  @Input()
-  EloPairs: EloDemoInput[];
+  public EloPairs = input.required<EloDemoInput[]>();
+  public Caption = input.required<string>();
 
-  @Input()
-  Caption: string;
+  protected matches = computed(() => this.EloPairs()
+    .map(pair => ({
+      matches: this.possibleOpponentScores.map(score => ({
+        p1NormalizedPoints: 9,
+        p1Elo: pair.elo1,
+        p2NormalizedPoints: score,
+        p2Elo: pair.elo2
+      })),
+      mode: pair.mode
+    }))
+    .map(batch => ({
+      elo1: batch.matches[0].p1Elo,
+      elo2: batch.matches[0].p2Elo,
+      s: batch.mode
+        ? batch.matches.map(match => EloFunctions.calculate(match, batch.mode))
+        : batch.matches.map(match => EloFunctions.calculate(match))
+    }))
+  )
 
-  @Input()
-  Mode: string;
-
-  ngOnInit() {
-    let matches = (this.EloPairs || [])
-      .map(pair => ({
-        matches: this.possibleOpponentScores.map(score => ({
-          p1NormalizedPoints: 9,
-          p1Elo: pair.elo1,
-          p2NormalizedPoints: score,
-          p2Elo: pair.elo2
-        })),
-        mode: pair.mode
-      }))
-      .map(batch => ({
-        elo1: batch.matches[0].p1Elo,
-        elo2: batch.matches[0].p2Elo,
-        s: batch.mode
-          ? batch.matches.map(match => EloFunctions.calculate(match, batch.mode))
-          : batch.matches.map(match => EloFunctions.calculate(match))
-      }));
-
-    this.dataSource = new MatTableDataSource<EloScoreDemo>(matches);
+  constructor() {
+    effect(() => this.dataSource = new MatTableDataSource<EloScoreDemo>(this.matches()));
   }
 }
 
